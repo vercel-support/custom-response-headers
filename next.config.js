c043d8fea0974537ea5eef2d822b991fce4d316c
Ismael Rumzan <help@qasid.com>
2022-03-22 13:06:04 +0300

    updated config

--- next.config.js
+++ next.config.js
@@ -4,7 +4,7 @@ const nextConfig = {
   async headers() {
     return [
       {
-        source: "/about",
+        source: "/cache-control",
         headers: [
           {
             key: "Cache-Control",
--- pages/cache-control.js
+++ pages/cache-control.js
@@ -21,35 +21,11 @@ export default function Home() {
           <code className={styles.code}>pages/index.js</code>
         </p>
 
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className={styles.card}
-          >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
+        <h2>Response Headers for Cache-Control</h2>
+        <ul>
+          <li>Go to `next.config.js`</li>
+          <li>Set the cache-control value to: s-maxage=1, stale-while-revalidate=59</li>
+        </ul>
       </main>
 
       <footer className={styles.footer}>
